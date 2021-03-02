c40833ea690a7564e95ec9a294e29311e7338221
salmoh-iu <78510342+salmoh-iu@users.noreply.github.com>
2021-03-01 20:34:14 -0500

    Create SpeedOfSound.java

---
+++ SpeedOfSound.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/*//
+   this program will show the speed of sound
+*/
+
+public class SpeedOfSound {
+   
+   public static void main(String[] args) {
+   
+   String input;  // to hold user input
+   
+   //create scanner object for keyboard input
+   Scanner keyboard = new Scanner(System.in);
+   
+   //prompt user to choose one element to determine speed of sound
+   System.out.println("Enter one element to determine their speed " +
+                        "of sound: ");
+   input = keyboard.nextLine();
+   
+   //display the diffrent elements
+   switch (input)
+   {
+   
+     
+  
+  
+  
+  }
+} 
